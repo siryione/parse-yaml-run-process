f94441ad1cf6b1443b7c88688f65b706adea285f
Ivan Siryi <siryione@gmail.com>
2022-04-10 14:25:16 +0300

    Remove redundant comments

--- main.cpp
+++ main.cpp
@@ -15,10 +15,8 @@ int main(){
         std::vector<Process> processes = config["processes"].as<std::vector<Process>>();
 
         for(Process p: processes){
-            // std::cout << p.getExe() << std::endl;
-            // p.run();
+            p.run();
         }
-        processes[0].run();
     }
     catch(const YAML::BadConversion& e){
         std::cerr << e.what() << '\n';
@@ -26,21 +24,3 @@ int main(){
     
     return 0;
 }
-
-
-
-// #include <boost/process.hpp>
-
-// #include <string>
-// #include <iostream>
-
-// // process command: ffmpeg -i in.webm out.opus
-// const std::string kInputFilePath = "/home/ivan_main/Downloads/sato.webm";
-
-// using namespace boost::process;
-
-// int main()
-// {
-//     int result = system(exe="/usr/bin/ffmpeg", args={"-i", kInputFilePath, "out.opus"});
-//     return 0;
-// }
--- process.cpp
+++ process.cpp
@@ -1,9 +1,3 @@
-// input: structure
-// var exe = structure.getExe()
-// var args = structure.getArgs()
-// var io_options = structure.prepare_io_options()
-// system(exe, args, io_options)
-
 #include "process.hpp"
 
 #include <filesystem>
